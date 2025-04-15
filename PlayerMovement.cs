4c9805b7828b07c98c44f9706de06691c00f6b9b
Kayla Cedeno <114173872+KaylaCedeno@users.noreply.github.com>
2025-04-15 17:18:55 -0400

    Update PlayerMovement.cs

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    // defined local variables
     public CharacterController controller;
 
     public float speed = 5;
@@ -16,24 +18,33 @@ public class PlayerMovement : MonoBehaviour
 
     Vector3 velocity;
     bool isGrounded;
+
+    
     void Update()
     {
+
+        // checks if the player is standing on the ground
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
+        // keeps player grounded if their velocity is < 0 (aka they are falling) + redirects to -2f
         if (isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
         }
 
+        // if the player uses the left shift key to move, then speed is set to 10
         if (Input.GetKey("left shift") && isGrounded)
         {
             speed = 10;
         }
+
+        // otherwise speed is set to 5
         else
         {
             speed = 5;
         }
 
+        // variables
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
@@ -41,13 +52,14 @@ public class PlayerMovement : MonoBehaviour
 
         controller.Move(move * speed * Time.deltaTime);
 
+        // if the jump button is pressed and the player is grounded..
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
+            // set velocity based on the jump height and gravity
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
 
         velocity.y += gravity * Time.deltaTime;
-
         controller.Move(velocity * Time.deltaTime);
     }
 }
